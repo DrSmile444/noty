a3e4b9b18e6cd0160f88299016a7974b5e9bb8cf
dmytro.vakulenko <dmytro.vakulenko@masterofcode.com>
2019-05-16 10:50:29 +0300

    added delay and symbol count options

--- main.js
+++ main.js
@@ -1,21 +1,58 @@
 const form = document.querySelector('[form]');
 const formTextArea = form.querySelector('[form-textarea]');
 const formButton = form.querySelector('[form-button]');
+const formDelayed = form.querySelector('[form-delayed]');
+const formSymbolCount = form.querySelector('[form-symbol-count]');
 
 formButton.addEventListener('click', handleButtonClick);
+formSymbolCount.value = 164; // default value
 
-function handleButtonClick() {
+function handleButtonClick(event) {
+    event.preventDefault();
+    let delayed = formDelayed.checked;
     let text = formTextArea.value.trim();
     text = removeExtraSymbols(text);
 
     if (text.length) {
-        cutMessage(text)
-            .forEach(createNoty);
 
-        formTextArea.value = '';
+        if (delayed) {
+            disableForm();
+
+            setTimeout(() => {
+                processButtonClick(text);
+                enableForm();
+            }, 2000);
+
+            return;
+        }
+
+        processButtonClick(text);
     }
 }
 
+function processButtonClick(text) {
+    cutMessage(text, formSymbolCount.value)
+        .forEach(createNoty);
+
+    formTextArea.value = '';
+}
+
+function disableForm() {
+    form.setAttribute('disabled', 'true');
+    formButton.setAttribute('disabled', 'true');
+    formTextArea.setAttribute('disabled', 'true');
+    formDelayed.setAttribute('disabled', 'true');
+    formSymbolCount.setAttribute('disabled', 'true');
+
+}
+
+function enableForm() {
+    form.removeAttribute('disabled');
+    formButton.removeAttribute('disabled');
+    formTextArea.removeAttribute('disabled');
+    formDelayed.removeAttribute('disabled');
+    formSymbolCount.removeAttribute('disabled');
+}
 
 function createNoty(body) {
     Noty.showNotification('', { body }).then(() => {
